751bce2e05c917ae992f2792d48cba8ed1601d46
Rusignuolo <casanostra@MacBook-di-Dario.local>
2013-02-02 18:41:14 +0100

    home ricerca...

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,11 +1,52 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	
-    
-	public function index()
-	{
-            $this->load->view('home');
-	}
+    public function index() {
+            $this->load->database();
+
+        if ($this->input->post('key') == false && $this->input->post('country') == false && $this->input->post('area') == false && $this->input->post('livello') == false) {
+            return $this->load->view('home', array(
+                        "country" => $this->db->query('select * from university group by country')->result(),
+                        "livello" => $this->db->query('select * from program_type')->result(),
+                        "area" => $this->db->query('select * from program group by sector')->result(),
+                        "courses" => null
+                    ));
+        }
+        $sector = "%";
+        if($this->input->post('livello')!= "") $sector = $this->input->post('livello'); 
+        
+        $country = "%";
+        if($this->input->post('country')!="") $country  = $this->input->post('country'); 
+        
+        $area = "%";
+        if($this->input->post('area')!="") $area = $this->input->post('area'); 
+        
+        $key = "%";
+        if($this->input->post('key')=="") $key = $this->input->post('key'); 
+        
+        $courses = $this->db->query("select * from program where sector like '".$sector."' and ".
+                                   "university_id in (select id from university where country like '".$country."') and".
+                                    " program_type_id in (select id from program_type where description like '".$area."') and ".
+                                    "name like '".$key."'")->result();
+        $c_final = array();
+
+        foreach($courses as $c){
+           $tmp = array();
+           $tmp['corso'] = $c;
+           $tmp['uni'] = $this->db->query("select * from university where id = ".$c->university_id)->first_row();
+        
+           array_push($c_final,$tmp);
+        }
+        return $this->load->view('home', array(
+                    "country" => $this->db->query('select * from university group by country')->result(),
+                    "livello" => $this->db->query('select * from program_type')->result(),
+                    "area" => $this->db->query('select * from program group by sector')->result(),
+                    "courses" => $c_final
+                ));
+    }
+
 }
--- application/views/home.php
+++ application/views/home.php
@@ -36,34 +36,54 @@
                 <div class="hero-unit">
                     <h1>The best way to make academic decisions!</h1>
                     <p>Find students feedback and choose the right program for you.</p>
-                    
-                    <form action="/index.php/uni/course" method="POST" id="search_form" class="form-horizontal">
+
+                    <form action="/index.php/home" method="post" id="search_form" class="form-horizontal">
                         <input type="text" placeholder="University, program, master ..." name="" />
-                        <input type="submit" class="btn btn-primary" value="Find" />
+                        <input type="submit" name="key" class="btn btn-primary" />
                         <a href="" id="adv_search">Advanced search</a>
                         <div id="div_adv_search" class="hide">
                             <label>Country</label>
-                            <select>
+                            <select name="country">
                                 <option></option>
-                                <option>Italia</option>
-                                <option>England</option>
+                                <?php
+                                foreach ($country as $c) {
+                                    echo "<option value=\"" . $c->country . "\">" . $c->country . "</option>";
+                                }
+                                ?>
                             </select>
                             <label>Area</label>
-                            <select>
+                            <select name="area">
                                 <option></option>
-                                <option>Business</option>
-                                <option>Design</option>
+                                <?php
+                                foreach ($area as $c) {
+                                    echo "<option value=\"" . $c->sector . "\">" . $c->sector . "</option>";
+                                }
+                                ?>
                             </select>
                             <label>Livello</label>
-                            <select>
+                            <select name="livello">
                                 <option></option>
-                                <option>PHD</option>
-                                <option>XXX</option>
+                                <?php
+                                foreach ($livello as $c) {
+                                    echo "<option value=\"" . $c->description . "\">" . $c->description . "</option>";
+                                }
+                                ?>
                             </select>
                         </div>
                     </form>
+                    <?php if ($courses!=null): ?>
+                        <h3>Lista dei corsi</h3>
+                        <ul>
+                            <?php
+                            foreach ($courses as $c) {
+                                echo "<li><a href=\"/index.php/uni/course/" . $c['corso']->id . "\">" . $c['corso']->name . "</a> presso <a href=\"/index.php/uni/index/" . $c['uni']->id . "\">" . $c['uni']->name . "</a></li>";
+                            }
+                            ?>
+                        </ul>
+                    <?php endif; ?>
+
                 </div>
-                
+
                 <div class="row">
                     <div class="span6">
                         <h4>SubHeading</h4>
@@ -76,15 +96,15 @@
                         <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
                     </div>
                     <div class="span6">
-                        
+
                         <div class="well">
                             <a href="https://www.facebook.com/pages/Ugrade/557436500934481" class="zocial facebook icon"></a>
                             <a href="https://twitter.com/Ugrade1" class="zocial twitter icon"></a>
                             <a href="mailto:ugrade.startup@gmail.com" class="zocial gmail icon"></a>
                             <a href="http://www.linkedin.com/groups/Ugrade-4833031/about" class="zocial linkedin icon"></a>
-                            
+
                             <hr />
-                            
+
                             <h4>Contribute</h4>
                             <p>
                                 Let us to improve our service
@@ -92,15 +112,15 @@
                             <a class="btn btn-large" href="http://www.surveymonkey.com/s/CNRQVVD" target="_blank">
                                 <i class="icon-ok-sign"></i> Join the survey
                             </a>
-                            
+
                             <hr />
-                            
+
                             <h4>Keep in touch</h4>
                             <a href="http://eepurl.com/uQE5v" target="_blank" class="btn btn-large"><i class="icon-envelope"></i> Subscribe our newsletter</a>
                         </div>
                     </div>
                 </div>
-                
+
                 <div class="footer">
                     <p>&copy; Ugrade 2013</p>
                 </div>
