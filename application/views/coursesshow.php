5732f38fee50e8b6c34835f688ecd522e5c5ff44
Christian Nastasi <christian.nastasi@gmail.com>
2013-02-02 19:57:40 +0100

    Inserimento commenti

--- application/views/coursesshow.php
+++ application/views/coursesshow.php
@@ -33,13 +33,51 @@
 
             <div class="container">
 
-                <?php echo $course['corso']->name; ?>
-                <?php echo $course['uni']->name; ?>
+                <?php echo $course['corso'] ->name; ?>
+                <?php echo $course['uni'  ] ->name; ?>
                 
+                <?php 
+                	if ( isset ( $message )) {
+						echo "<div id='message'>$message</div>\n";	
+					}
+                ?>
+                
+                <form action="/uni/send_comment" method="POST" >
+                	<?php
+                		$counter = 0; 
+                		if (isset  ( $course) ) {
+                			foreach ($course["questions"] as $question) {  
+					?>
+                		
+                	E-mail <input type="text" name="email">
+               		<div>
+               			<input type="hidden" name="id<?php echo $counter; ?>" value="<?php echo $question -> id;?>">
+               			<div>
+                			<div><?php echo $question -> titolo; ?></div>
+                			<div><?php echo $question -> text; ?></div>
+                		</div>
+                		<div>
+                			<input type="radio" name="vote<?php echo $counter; ?>" value="1">1
+							<input type="radio" name="vote<?php echo $counter; ?>" value="2">2
+							<input type="radio" name="vote<?php echo $counter; ?>" value="3" checked>3
+							<input type="radio" name="vote<?php echo $counter; ?>" value="4">4
+							<input type="radio" name="vote<?php echo $counter; ?>" value="5">5<br/>
+							<textarea name="comment<?php echo $counter; ?>"></textarea>
+                		</div>
+                	</div>
+                	<?php
+                				$counter ++; 
+                			}
+						} 
+					?>
+					
+					<input type="hidden" name="counter" value="<?php echo $counter;?>">
+					<input type="hidden" name="program_id" value="<?php echo $course["program_id"]; ?>">
+					<input type="submit" >
+                </form>      
                 <div class="footer">
                     <p>&copy; Company 2013</p>
                 </div>
-
             </div>
         </div>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
